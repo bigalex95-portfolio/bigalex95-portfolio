fa6f61b901556cc9db95bc77bbafb7928fb0232d
bigalex95 <alibek060395@gmail.com>
2021-10-26 13:00:08 +0300

    change description metadata

--- gatsby-theme-cara/gatsby-config.js
+++ gatsby-theme-cara/gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = ({ basePath = `/`, mdx = true }) => ({
     siteTitleAlt: `Cara - @lekoarts/gatsby-theme-cara`,
     siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
     siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteDescription: `My Personal Portfolio with Live Demo of Developed Projects.`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
     author: `@lekoarts_de`,
